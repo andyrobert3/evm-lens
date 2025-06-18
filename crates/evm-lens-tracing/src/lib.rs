b4c60d886fb1719f7649c66a3cea0d393fbd8915
zianksm <mynameiszian@gmail.com>
2025-06-18 22:20:57 +0800

    feat: partially add tracing logic

--- crates/evm-lens-tracing/src/lib.rs
+++ crates/evm-lens-tracing/src/lib.rs
@@ -1,20 +1,28 @@
 // fetch tx data
+// fetch block
+// run all tx until before the target tx
+// use that as the db state
+// wrap tracer inspector
 // run inside revm
-// custom backend to fetch block info and storage slots
 // output tracing info
+// collect and segregate traces to it's individual calls
 // display stuff
 
-pub mod backend;
-
 use alloy::{
+    eips::{BlockId, BlockNumberOrTag},
     network::Ethereum,
     primitives::TxHash,
     providers::{Provider, ProviderBuilder},
-    rpc::types::Transaction,
+    rpc::types::{Block, Transaction},
     transports::{RpcError, TransportErrorKind},
 };
+use revm::{database::{AlloyDB, CacheDB, StateBuilder}, database_interface::WrapDatabaseAsync};
 
-pub struct Tracer<P: Provider> {
+#[derive(Clone, Debug)]
+pub struct Tracer<P>
+where
+    P: Provider + Clone,
+{
     provider: P,
 }
 
@@ -24,13 +32,18 @@ pub enum TracingError {
     Connection(String),
     #[error("{0}")]
     Io(#[from] RpcError<TransportErrorKind>),
-    #[error("Invalid transaction")]
+    #[error("Invalid transaction or block")]
     Invalid,
+    #[error("{0}")]
+    Other(String),
 }
 
 pub type TracingResult<T> = Result<T, TracingError>;
 
-impl<T: Provider> Tracer<T> {
+impl<T> Tracer<T>
+where
+    T: Provider + Clone,
+{
     pub fn new(provider: T) -> Self {
         Self { provider }
     }
@@ -41,6 +54,37 @@ impl<T: Provider> Tracer<T> {
             .await
             .map_err(|err| TracingError::Io(err))?
             .ok_or(TracingError::Invalid)
+    }
+
+    async fn fetch_block_full(&self, block: BlockNumberOrTag) -> TracingResult<Block> {
+        self.provider
+            .get_block_by_number(block)
+            .full()
+            .await
+            .map_err(|err| TracingError::Io(err))?
+            .ok_or(TracingError::Invalid)
+    }
+
+    pub async fn trace(&self, hash: TxHash) -> TracingResult<()> {
+        let tx = self.fetch_tx_data(hash).await?;
+
+        let Some(block) = &tx.block_number else {
+            return Err(TracingError::Other(String::from(
+                "Can't trace pending transaction",
+            )));
+        };
+        let block_ident = BlockNumberOrTag::Number(block.to_owned());
+        let block = self.fetch_block_full(block_ident).await?;
+
+        let state_db = AlloyDB::new(self.provider.clone(), BlockId::Number(block_ident));
+        let state_db = WrapDatabaseAsync::new(state_db).ok_or(TracingError::Other(format!(
+            "for some reason no tokio rt is found :("
+        )))?;
+        let state_db = CacheDB::new(state_db);
+        let mut state = StateBuilder::new_with_database(state_db).build();
+
+
+        todo!()
     }
 }
 
