4e7a418002093ccfc004153f36b04dfc3e0260d8
zianksm <mynameiszian@gmail.com>
2025-06-20 22:11:45 +0800

    feat: partially impl tracing items

--- crates/evm-lens-tracing/src/lib.rs
+++ crates/evm-lens-tracing/src/lib.rs
@@ -8,15 +8,66 @@
 // collect and segregate traces to it's individual calls
 // display stuff
 
+use std::io::Write;
+
 use alloy::{
+    consensus::Transaction,
     eips::{BlockId, BlockNumberOrTag},
     network::Ethereum,
     primitives::TxHash,
     providers::{Provider, ProviderBuilder},
-    rpc::types::{Block, Transaction},
+    rpc::types::{Block, BlockTransactions, Transaction},
     transports::{RpcError, TransportErrorKind},
 };
-use revm::{database::{AlloyDB, CacheDB, StateBuilder}, database_interface::WrapDatabaseAsync};
+use revm::{
+    database::{AlloyDB, CacheDB, StateBuilder}, database_interface::WrapDatabaseAsync, inspector::inspectors::TracerEip3155, primitives::U256, Context, MainBuilder, MainContext
+};
+
+use crate::sort::SortMarker;
+
+pub mod item;
+
+
+pub mod sort {
+
+    pub struct Sorted;
+    pub struct Unsorted;
+    
+    pub trait SortMarker {}
+    impl SortMarker for Sorted{}
+    impl SortMarker for Unsorted{}
+    
+}
+
+/// used to collect traces from inspector
+#[derive(Clone)]
+pub struct Traces<S: sort::SortMarker>{
+    buff : Vec<>
+}
+
+pub enum TraceKind {
+Summary()
+}
+
+
+impl<S:SortMarker> Write for Traces<S> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        // handles new line being written by the tracer
+        // we don't actually need the new line since we're writing to memory
+        if buf.len() == 1 {
+            return Ok(1)
+        }
+
+
+
+
+        
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        todo!()
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct Tracer<P>
@@ -66,6 +117,7 @@ where
     }
 
     pub async fn trace(&self, hash: TxHash) -> TracingResult<()> {
+        let chain_id = self.provider.get_chain_id().await?;
         let tx = self.fetch_tx_data(hash).await?;
 
         let Some(block) = &tx.block_number else {
@@ -83,9 +135,54 @@ where
         let state_db = CacheDB::new(state_db);
         let mut state = StateBuilder::new_with_database(state_db).build();
 
+        let ctx = Context::mainnet()
+            .with_db(&mut state)
+            .modify_block_chained(|b| {
+                b.number = block.header.number;
+                b.beneficiary = block.header.beneficiary;
+                b.timestamp = block.header.timestamp;
+
+                b.difficulty = block.header.difficulty;
+                b.gas_limit = block.header.gas_limit;
+                b.basefee = block.header.base_fee_per_gas.unwrap_or_default();
+            })
+            .modify_cfg_chained(|c| {
+                c.chain_id = chain_id;
+            });
+
+            
+                let mut evm = ctx.build_mainnet_with_inspector(TracerEip3155::new(Box::new(writer)));
+
+        let BlockTransactions::Full(transactions) = block.transactions else {
+            return Err(TracingError::Invalid);
+        };
+
+        for tx in transactions {
+            // Construct the file writer to write the trace to
+            let tx_number = tx.transaction_index.unwrap_or_default();
+
+            let tx = TxEnv {
+                caller: tx.inner.signer(),
+                gas_limit: tx.gas_limit(),
+                gas_price: tx.gas_price().unwrap_or(tx.inner.max_fee_per_gas()),
+                value: tx.value(),
+                data: tx.input().to_owned(),
+                gas_priority_fee: tx.max_priority_fee_per_gas(),
+                chain_id: Some(chain_id),
+                nonce: tx.nonce(),
+                access_list: tx.access_list().cloned().unwrap_or_default(),
+                kind: match tx.to() {
+                    Some(to_address) => TxKind::Call(to_address),
+                    None => TxKind::Create,
+                },
+                ..Default::default()
+            };
+        }
 
         todo!()
     }
+
+    async fn inspect() {}
 }
 
 pub async fn create_provider(url: &str) -> TracingResult<impl Provider> {
